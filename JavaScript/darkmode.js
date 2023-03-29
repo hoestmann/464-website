0e0b86bc86a4e8489afcea82e8fd365c9071243b
Hunter Oestmann <hoestmann2@huskers.unl.edu>
2023-03-29 10:11:43 -0500

    Changes made day of class.

--- JavaScript/darkmode.js
+++ JavaScript/darkmode.js
@@ -1,10 +1,17 @@
+var darkModeEnabled = false;
+
 function toggleDarkMode() {
     console.log("Toggle Dark Mode");
     var body = document.body;
     body.classList.toggle("dark-theme");
+    if (darkModeEnabled) {
+        darkModeEnabled = false
+    } else {
+        darkModeEnabled = true
+    }
 }
 
 window.onload = function() {
     var body = document.body;
-    body.classList.add("light-theme");
+    //body.classList.add("light-theme");
 };
